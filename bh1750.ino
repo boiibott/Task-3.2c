2485cb92607026baa50a2baf4231aa65f407beb3
boiibott <132186370+boiibott@users.noreply.github.com>
2023-10-01 00:15:45 +0530

    Update bh1750.ino

--- bh1750.ino
+++ bh1750.ino
@@ -1,84 +1,73 @@
 #include <BH1750.h>
-
 #include <Wire.h>
-
 #include <WiFiNINA.h>
 
-// Replace with your network credentials
-char ssid[] = "Free virus";
-char pass[] = "mask90009000";
+BH1750 lightMeter;
+float lux;
+
+// WiFi credentials
+char ssid[] = "M30s";
+char pass[] = "vcvw3532";
 
 WiFiClient client;
 
 char   HOST_NAME[] = "maker.ifttt.com";
-String PATH_NAME   = "https://maker.ifttt.com/trigger/Light_intensity/json/with/key/ofHfOy24U8Z572xy8SHdtZm97CE8nfjBDo4YKZq9o4J"; // change your EVENT-NAME and YOUR-KEY
-
-// Create an instance of the BH1750 sensor
-BH1750 lightSensor;
+String PATH_NAME = "/trigger/light/json/with/key/ofHfOy24U8Z572xy8SHdtZm97CE8nfjBDo4YKZq9o4J"; // change your EVENT-NAME and YOUR-KEY
+String queryString;
 
 void setup() {
-  // Initialize serial communication
   Serial.begin(9600);
-  while (!Serial);
 
-  // Initialize the BH1750 sensor
-  if (!lightSensor.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
-    Serial.println("BH1750 initialization failed!");
-    while (1);
-  }
+  // Initialize the I2C bus (BH1750 library doesn't do this automatically)
+  Wire.begin();
+
+  lightMeter.begin();
+  Serial.println(F("BH1750 Test begin"));
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, pass);
-  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
   }
-  Serial.println("\nConnected to WiFi");
+
+  Serial.println("Connected to WiFi");
 }
 
 void loop() {
-  // Read light intensity in lux
-  float lux = lightSensor.readLightLevel();
+  lux = lightMeter.readLightLevel(); // Read light intensity
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
 
-  // Define a threshold value for light intensity
-  float thresholdLux = 500.0;
+  if (Serial.read() == 's') {
+    // Get the current timestamp in the desired format
+    String timestamp = "October 1, 2023 at 12:06 AM";
 
-  // Check if light intensity exceeds the threshold
-  if (lux > thresholdLux) {
-    sendIFTTTNotification(lux);
-  }
+    // Update the queryString with the current lux value and timestamp
+    queryString = "?value1=" + String(lux) + "&value2=" + timestamp;
 
-  // Delay before next reading
-  delay(60000); // Delay for 1 minute
-}
+    // Make an HTTP request to the IFTTT webhook:
+    if (client.connect(HOST_NAME, 80)) {
+      // Send HTTP header
+      client.println("GET " + PATH_NAME + queryString + " HTTP/1.1");
+      client.println("Host: " + String(HOST_NAME));
+      client.println("Connection: close");
+      client.println(); // End HTTP header
 
-void sendIFTTTNotification(float luxValue) {
-  if (client.connect(HOST_NAME, 80)) {
-    // Send HTTP request
-    client.print("GET " + PATH_NAME);
-    client.print("?value1=");
-    client.print(luxValue);
-    client.println(" HTTP/1.1");
-    client.println("Host: " + String(HOST_NAME));
-    client.println("Connection: close");
-    client.println();
+      while (client.connected()) {
+        if (client.available()) {
+          // Read an incoming byte from the server and print it to the Serial Monitor:
+          char c = client.read();
+          Serial.print(c);
+        }
+      }
 
-    Serial.println("IFTTT Notification Sent");
-  } else {
-    Serial.println("Unable to connect to IFTTT");
-  }
-
-  // Wait for server response
-  while (client.connected()) {
-    if (client.available()) {
-      // Read and print the response from the server
-      char c = client.read();
-      Serial.print(c);
+      // The server's disconnected, stop the client:
+      client.stop();
+      Serial.println();
+      Serial.println("Disconnected");
     }
   }
-
-  // Disconnect from the server
-  client.stop();
-  Serial.println("\nDisconnected");
+  delay(2000);
 }
