ad66e79291ace6bc487fb57eb73ae5f00df9731e
boiibott <132186370+boiibott@users.noreply.github.com>
2023-10-01 01:08:21 +0530

    Update bh1750.ino

--- bh1750.ino
+++ bh1750.ino
@@ -1,53 +1,46 @@
+#include <WiFiNINA.h>
 #include <BH1750.h>
 #include <Wire.h>
-#include <WiFiNINA.h>
 
+char ssid[] = "M30s";      // Replace with your Wi-Fi SSID
+char pass[] = "vcvw3532";  // Replace with your Wi-Fi password
+
+WiFiClient client; 
 BH1750 lightMeter;
-float lux;
 
-// WiFi credentials
-char ssid[] = "M30s";
-char pass[] = "vcvw3532";
-
-WiFiClient client;
-
-char   HOST_NAME[] = "maker.ifttt.com";
-String PATH_NAME = "/trigger/light/json/with/key/ofHfOy24U8Z572xy8SHdtZm97CE8nfjBDo4YKZq9o4J"; // change your EVENT-NAME and YOUR-KEY
-String queryString;
+char HOST_NAME[] = "maker.ifttt.com";
+String PATH_NAME = "/trigger/light/json/with/key/ofHfOy24U8Z572xy8SHdtZm97CE8nfjBDo4YKZq9o4J";
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial);
 
-  // Initialize the I2C bus (BH1750 library doesn't do this automatically)
+  WiFi.begin(ssid, pass);
   Wire.begin();
 
-  lightMeter.begin();
-  Serial.println(F("BH1750 Test begin"));
-
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
 
   Serial.println("Connected to WiFi");
+  lightMeter.begin();
 }
 
 void loop() {
-  lux = lightMeter.readLightLevel(); // Read light intensity
+  float lux = lightMeter.readLightLevel();
+
   Serial.print("Light: ");
   Serial.print(lux);
   Serial.println(" lx");
 
-  if (Serial.read() == 's') {
+  if (lux > 100 && Serial.read() == 's') {
+    String queryString = "?value1=" + String(lux);
+    
     // Get the current timestamp in the desired format
     String timestamp = "October 1, 2023 at 12:06 AM";
 
-    // Update the queryString with the current lux value and timestamp
-    queryString = "?value1=" + String(lux) + "&value2=" + timestamp;
-
-    // Make an HTTP request to the IFTTT webhook:
+    //Make an HTTP request to the IFTTT webhook:
     if (client.connect(HOST_NAME, 80)) {
       // Send HTTP header
       client.println("GET " + PATH_NAME + queryString + " HTTP/1.1");
@@ -68,6 +61,7 @@ void loop() {
       Serial.println();
       Serial.println("Disconnected");
     }
+
   }
-  delay(2000);
+  delay(3000);
 }
